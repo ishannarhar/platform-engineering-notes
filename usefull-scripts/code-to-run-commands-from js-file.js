0a64ed4a4b375ca10fca68bf0b8f0dd9bad8e978
Ishan Narhar <ishan9021@gmail.com>
2026-07-03 10:50:20 +0530

    Update code-to-run-commands-from js-file.js

--- usefull-scripts/code-to-run-commands-from js-file.js
+++ usefull-scripts/code-to-run-commands-from js-file.js
@@ -1,1 +1,17 @@
+//Importing export sync to run commands in the commandline
+const {execSync} = require("child_process")
 
+//List the name of the services you want to test
+const services = {
+  serviceOne:"Actual Service Name",
+  serviceTwo:"Another Service Name"
+}
+
+//From commandline args, we extract the service name and the environment
+const [serviceKey, env = "nonprod"] = process.argv.slice(2)
+
+//In case the user does not pass the service name, we list the service key names for user's reference
+if(!serviceKey){
+  console.log("Available Services: ")
+  Object.keys(services).forEach((key)=> console.log(`  -${key}`))
+}
